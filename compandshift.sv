2a80e18e634d07cc1e71bb4d1ef6be30e0bf9cdd
Krishna S <103890297+haxthehacc@users.noreply.github.com>
2024-11-18 19:51:34 +0530

    Update compandshift.sv

--- compandshift.sv
+++ compandshift.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.11.2024 14:36:30
-// Design Name: 
-// Module Name: compandshift
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module compandshift(cas_mantissa_1, cas_mantissa_2, 
                     cas_exponent_1, cas_exponent_2, clk, reset, 
