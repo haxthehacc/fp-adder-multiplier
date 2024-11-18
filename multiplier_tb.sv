1d48641cd9e7e751dd69946dd33ff74cf0eb7645
Krishna S <103890297+haxthehacc@users.noreply.github.com>
2024-11-18 19:51:56 +0530

    Update multiplier_tb.sv

--- multiplier_tb.sv
+++ multiplier_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.11.2024 10:46:04
-// Design Name: 
-// Module Name: multiplier_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module multiplier_tb();
 
